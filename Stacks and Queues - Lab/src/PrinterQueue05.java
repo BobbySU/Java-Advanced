927b6f6b795aa7ba92c96d1c76e0390e9be0a228
BobbySU <radio_20bg@yahoo.com>
2021-09-13 21:44:46 +0300

    New PrinterQueue05

--- Stacks and Queues - Lab/src/PrinterQueue05.java
+++ Stacks and Queues - Lab/src/PrinterQueue05.java
@@ -1,8 +1,27 @@
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class PrinterQueue05 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        ArrayDeque<String> queue = new ArrayDeque<>();
+        String input = scan.nextLine();
+        while (!input.equals("print")) {
+            if (input.equals("cancel")) {
+                if (queue.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    System.out.printf("Canceled %s%n", queue.peek());
+                    queue.poll();
+                }
+            } else {
+                queue.offer(input);
+            }
+            input = scan.nextLine();
+        }
+        for (String str : queue) {
+            System.out.println(str);
+        }
     }
 }
