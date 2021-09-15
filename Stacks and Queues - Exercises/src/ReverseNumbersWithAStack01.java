780d495adb675f45ddb659a6643b112449dcbc6b
BobbySU <radio_20bg@yahoo.com>
2021-09-15 23:31:53 +0300

    Update Reverse Numbers with a Stack

--- Stacks and Queues - Exercises/src/ReverseNumbersWithAStack01.java
+++ Stacks and Queues - Exercises/src/ReverseNumbersWithAStack01.java
@@ -1,9 +1,15 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReverseNumbersWithAStack01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        Arrays.stream(scan.nextLine().split("\\s+")).forEach(e -> stack.push(Integer.parseInt(e)));
+        for (Integer e : stack) {
+            System.out.print(stack.pop() + " ");
+        }
     }
 }
