0c8b52baedb3d0a1ffa187c25b12a0e47f5e6fe9
BobbySU <radio_20bg@yahoo.com>
2021-09-15 20:48:16 +0300

    Update MathPotato07

--- Stacks and Queues - Lab/src/MathPotato07.java
+++ Stacks and Queues - Lab/src/MathPotato07.java
@@ -13,7 +13,33 @@ public class MathPotato07 {
         for (String e : kids) {
             queue.offer(e);
         }
+        int cycle = 1;
+        while (queue.size() > 1) {
+            for (int i = 1; i < n; i++) {
+                queue.offer(queue.poll());
+            }
+            if (isPrime(cycle)) {
+                System.out.println("Prime " + queue.peek());
+            } else {
+                System.out.printf("Removed %s%n", queue.poll());
+            }
+            cycle++;
+        }
+        System.out.printf("Last is %s%n", queue.poll());
+    }
 
-        
+    private static boolean isPrime(int n) {
+        boolean primeCheck = true;
+        if (n == 0 || n == 1) {
+            primeCheck = false;
+            return primeCheck;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    primeCheck = false;
+                }
+            }
+            return primeCheck;
+        }
     }
 }
