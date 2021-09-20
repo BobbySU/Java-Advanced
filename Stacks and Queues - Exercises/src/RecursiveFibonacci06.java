f7e9e11d692cd0249c976eec70026d0bcd3f4a93
BobbySU <radio_20bg@yahoo.com>
2021-09-20 21:18:56 +0300

    Add Recursive Fibonacci

--- Stacks and Queues - Exercises/src/RecursiveFibonacci06.java
+++ Stacks and Queues - Exercises/src/RecursiveFibonacci06.java
@@ -1,11 +1,23 @@
 import java.util.Scanner;
 
 public class RecursiveFibonacci06 {
+    public static long[] memoryFib;
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int n = scan.nextInt();
+        memoryFib = new long[n + 1];
+        System.out.println(fib(n));
+    }
 
-
-
+    public static long fib(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        if (memoryFib[n] != 0) {
+            return memoryFib[n];
+        }
+        return memoryFib[n] = fib(n - 1) + fib(n - 2);
     }
 }
