c993ffa4d30e4a78207d0d6525719198eeec2b99
BobbySU <radio_20bg@yahoo.com>
2021-09-21 00:45:34 +0300

    Add Multidimensional Arrays - Lab and Compare Matrices

---
+++ Multidimensional Arrays - Lab/src/CompareMatrices01.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CompareMatrices01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int row = Integer.parseInt(scan.nextLine().split("\\s+")[0]);
+        int[][] first = readMatrix(row, scan);
+        row = Integer.parseInt(scan.nextLine().split("\\s+")[0]);
+        int[][] second = readMatrix(row, scan);
+
+        boolean areEqual = areEqual(first, second);
+
+        String output = areEqual ? "equal" : "not equal";
+        System.out.println(output);
+    }
+
+    private static boolean areEqual(int[][] first, int[][] second) {
+        if (first.length != second.length) {
+            return false;
+        }
+        for (int i = 0; i < first.length; i++) {
+            int[] firstArr = first[i];
+            int[] secondArr = second[i];
+            if (firstArr.length != secondArr.length) {
+                return false;
+            }
+            for (int j = 0; j < firstArr.length; j++) {
+                if (firstArr[j] != secondArr[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static int[][] readMatrix(int rows, Scanner scan) {
+        int[][] matrix = new int[rows][];
+        for (int row = 0; row < rows; row++) {
+            int[] arr = Arrays.stream(scan.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            matrix[row] = arr;
+        }
+        return matrix;
+    }
+}
