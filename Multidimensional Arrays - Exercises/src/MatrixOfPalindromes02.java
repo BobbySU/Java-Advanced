a39df2f6609025853b8436cc901aea614ca4389f
BobbySU <radio_20bg@yahoo.com>
2021-09-23 23:34:54 +0300

    Update Matrix of Palindromes

--- Multidimensional Arrays - Exercises/src/MatrixOfPalindromes02.java
+++ Multidimensional Arrays - Exercises/src/MatrixOfPalindromes02.java
@@ -7,11 +7,18 @@ public class MatrixOfPalindromes02 {
         String[] input = scan.nextLine().split(" ");
         String[][] matrix = new String[Integer.parseInt(input[0])][Integer.parseInt(input[1])];
 
-
-
+        char letter = 'a';
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                String palindrome = "" + letter + (char) (letter + j) + letter;
+                matrix[i][j] = palindrome;
+            }
+            letter = (char) (letter + 1);
+        }
+        printMatrix(matrix);
     }
 
-    private static void printMatrix(int[][] matrix) {
+    private static void printMatrix(String[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
