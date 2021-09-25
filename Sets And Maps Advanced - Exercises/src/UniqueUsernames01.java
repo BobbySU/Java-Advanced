89cb90d8e280a6a9ced42e05d07d8526f6c265b5
BobbySU <radio_20bg@yahoo.com>
2021-09-25 21:46:01 +0300

    Add Unique Usernames

--- Sets And Maps Advanced - Exercises/src/UniqueUsernames01.java
+++ Sets And Maps Advanced - Exercises/src/UniqueUsernames01.java
@@ -1,9 +1,17 @@
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class UniqueUsernames01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-
+        int n = Integer.parseInt(scan.nextLine());
+        Set<String> name = new LinkedHashSet<>();
+        for (int i = 0; i < n; i++) {
+            String input = scan.nextLine();
+            name.add(input);
+        }
+        name.forEach(System.out::println);
     }
 }
