fe2f5abbfe447cf24bc90731ad959c5abd9194c9
BobbySU <radio_20bg@yahoo.com>
2021-09-28 02:45:28 +0300

    Update Read File

--- Streams, Files And Directories - Lab/src/ReadFile01.java
+++ Streams, Files And Directories - Lab/src/ReadFile01.java
@@ -1,3 +1,22 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ReadFile01 {
-    
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+
+        String path = "C:\\Users\\BOBBY\\Desktop\\04. Java-Advanced-Streams-Files-and-Directories-Resources\\input.txt";
+        try {
+            FileInputStream in = new FileInputStream(path);
+
+            int var = in.read();
+            while (var != -1) {
+                System.out.print(var + " ");
+                var = in.read();
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
