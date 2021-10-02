74e040e623ae53858614fa629a730ef8a105f85b
BobbySU <radio_20bg@yahoo.com>
2021-10-02 23:50:18 +0300

    Add Line Numbers

--- Streams, Files And Directories - Exercises/src/LineNumbers05.java
+++ Streams, Files And Directories - Exercises/src/LineNumbers05.java
@@ -1,7 +1,19 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class LineNumbers05 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
-
-
+        List<String> input = Files.readAllLines(Path.of("inputLineNumbers.txt"));
+        PrintWriter printWriter = new PrintWriter("output5.txt");
+        int count = 1;
+        for (String e : input) {
+            printWriter.println(count + ". " + e);
+            count++;
+        }
+        printWriter.close();
     }
 }
