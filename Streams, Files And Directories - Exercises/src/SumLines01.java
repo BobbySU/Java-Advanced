943ccb61c38a602071fcbdc42358db133e881925
BobbySU <radio_20bg@yahoo.com>
2021-10-02 21:42:26 +0300

    Add Sum Lines

--- Streams, Files And Directories - Exercises/src/SumLines01.java
+++ Streams, Files And Directories - Exercises/src/SumLines01.java
@@ -1,8 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class SumLines01 {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
+        Path path = Path.of("input.txt");
 
+        List<String> allLines = Files.readAllLines(path);
 
-
+        for (String e : allLines) {
+            int sum = 0;
+            for (int i = 0; i < e.length(); i++) {
+                char in = e.charAt(i);
+                sum += in;
+            }
+            System.out.println(sum);
+        }
     }
 }
