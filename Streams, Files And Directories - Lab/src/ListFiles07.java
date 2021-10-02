02252d5c5dd0f39337e14070043eb9f6e4332c29
BobbySU <radio_20bg@yahoo.com>
2021-10-02 20:49:23 +0300

    Add List Files

--- Streams, Files And Directories - Lab/src/ListFiles07.java
+++ Streams, Files And Directories - Lab/src/ListFiles07.java
@@ -1,9 +1,20 @@
-import java.io.IOException;
+import java.io.File;
 
 public class ListFiles07 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
+        File file = new File("C:\\Users\\BOBBY\\IdeaProjects\\GitHub\\Java-Advanced\\Streams, Files And Directories - Lab\\Files-and-Streams");
 
-
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                File[] files = file.listFiles();
+                for (File f : files) {
+                    if (!f.isDirectory()) {
+                        System.out.printf("%s: [%s]%n",
+                                f.getName(), f.length());
+                    }
+                }
+            }
+        }
     }
 }
