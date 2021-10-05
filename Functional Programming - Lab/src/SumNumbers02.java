46b47fbde57c1953e79df9e6209178f0a574530c
BobbySU <radio_20bg@yahoo.com>
2021-10-05 20:49:31 +0300

    Add Sum Numbers

--- Functional Programming - Lab/src/SumNumbers02.java
+++ Functional Programming - Lab/src/SumNumbers02.java
@@ -1,11 +1,19 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class SumNumbers02 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        String in = scan.nextLine();
 
+        Function<String, Integer> count = e -> e.split(", ").length;
+        Function<String, Integer> sum = e -> Arrays.stream(e.split(", "))
+                .mapToInt(Integer::parseInt).sum();
 
+        System.out.println("Count = " + count.apply(in));
+        System.out.println("Sum = " + sum.apply(in));
 
     }
 }
