0bc182110c5b2b505e5771ae29a801fbcb5cce58
BobbySU <radio_20bg@yahoo.com>
2021-10-05 21:15:05 +0300

    Add Count Uppercase Words

---
+++ Functional Programming - Lab/src/CountUppercaseWords03.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class CountUppercaseWords03 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String[] in = scan.nextLine().split("\\s+");
+
+        Predicate<String> checkUpperCase = e -> Character.isUpperCase(e.charAt(0));
+
+        List<String> upper = Arrays.stream(in).filter(checkUpperCase).collect(Collectors.toList());
+
+        System.out.println(upper.size());
+        System.out.println(String.join(System.lineSeparator(), upper));
+    }
+}
