d18a4c7cef68444fd5cdf7e6b1ecfcfefc16ff49
BobbySU <radio_20bg@yahoo.com>
2021-10-05 20:12:05 +0300

    Update with Function

--- Functional Programming - Lab/src/SortEvenNumbers01.java
+++ Functional Programming - Lab/src/SortEvenNumbers01.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SortEvenNumbers01 {
     public static void main(String[] args) {
@@ -14,10 +16,12 @@ public class SortEvenNumbers01 {
         List<Integer> evenNum = in.stream().filter(e -> e % 2 == 0)
                 .collect(Collectors.toList());
 
-        String out = evenNum.stream().map(String::valueOf).collect(Collectors.joining(", "));
+        Function<Stream<Integer>,String> mutator = a->a.map(String::valueOf).collect(Collectors.joining(", "));
+
+        String out = mutator.apply(evenNum.stream());
         System.out.println(out);
 
-        String out2 = evenNum.stream().sorted().map(String::valueOf).collect(Collectors.joining(", "));
+        String out2 = mutator.apply(evenNum.stream().sorted());
         System.out.println(out2);
     }
 }
