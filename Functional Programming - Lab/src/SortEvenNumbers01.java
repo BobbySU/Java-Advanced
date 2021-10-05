0c1e1243297321f45e46890e987f12afdada7e90
BobbySU <radio_20bg@yahoo.com>
2021-10-05 20:03:14 +0300

    Add Sort Even Numbers

--- Functional Programming - Lab/src/SortEvenNumbers01.java
+++ Functional Programming - Lab/src/SortEvenNumbers01.java
@@ -1,3 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class SortEvenNumbers01 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        List<Integer> in = Arrays.stream(scan.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Integer> evenNum = in.stream().filter(e -> e % 2 == 0)
+                .collect(Collectors.toList());
+
+        String out = evenNum.stream().map(String::valueOf).collect(Collectors.joining(", "));
+        System.out.println(out);
+
+        String out2 = evenNum.stream().sorted().map(String::valueOf).collect(Collectors.joining(", "));
+        System.out.println(out2);
+    }
 }
