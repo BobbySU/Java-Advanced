73c1ecc859d3a523657aecaaa741335fed2d4c65
BobbySU <radio_20bg@yahoo.com>
2021-10-08 00:37:24 +0300

    Add Opinion Poll

--- Defining Classes - Exercises/src/OpinionPoll01/Main.java
+++ Defining Classes - Exercises/src/OpinionPoll01/Main.java
@@ -1,13 +1,23 @@
 package OpinionPoll01;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        List<Person> peoples = new ArrayList<>();
 
-
-        
+        int n = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < n; i++) {
+            String[] input = scan.nextLine().split("\\s+");
+            Person person = new Person(input[0], Integer.parseInt(input[1]));
+            peoples.add(person);
+        }
+        peoples.stream().filter(e -> e.getAge() > 30).sorted(Comparator.comparing(Person::getName))
+                .forEach(e -> System.out.println(e.getName() + " - " + e.getAge()));
     }
 }
