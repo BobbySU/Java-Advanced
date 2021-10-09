a6078960a5e40a590bf62394cb7faf1a08c0bdbf
BobbySU <radio_20bg@yahoo.com>
2021-10-09 23:47:59 +0300

    Add Speed Racing

---
+++ Defining Classes - Exercises/src/SpeedRacing03/Main.java
@@ -0,0 +1,14 @@
+package SpeedRacing03;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+
+    }
+}
