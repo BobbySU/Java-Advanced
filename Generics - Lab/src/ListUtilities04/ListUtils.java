c40ba846a305d099c1e668123b84118f41c445da
BobbySU <radio_20bg@yahoo.com>
2021-10-18 21:45:15 +0300

    Add List Utilities

---
+++ Generics - Lab/src/ListUtilities04/ListUtils.java
@@ -0,0 +1,24 @@
+package ListUtilities04;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ListUtils {
+
+    public static <T extends Comparable<T>> T getMin(List<T> elements) {
+        checkForEmpty(elements);
+        return Collections.min(elements);
+    }
+
+    public static <T extends Comparable<T>> T getMax(List<T> elements) {
+        checkForEmpty(elements);
+        return Collections.max(elements);
+    }
+
+    private static <T> T checkForEmpty(List<T> elements) {
+        if (elements.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        return null;
+    }
+}
---
+++ Generics - Lab/src/ListUtilities04/Main.java
@@ -0,0 +1,7 @@
+package ListUtilities04;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
