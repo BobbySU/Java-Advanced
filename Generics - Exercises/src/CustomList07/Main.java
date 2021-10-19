b6c2ae1dfe18451456eef3f883721444b58cef2a
BobbySU <radio_20bg@yahoo.com>
2021-10-19 21:41:08 +0300

    Add Custom List

---
+++ Generics - Exercises/src/CustomList07/Main.java
@@ -0,0 +1,45 @@
+package CustomList07;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        CustomList<String> list = new CustomList<String>();
+
+        String input = scan.nextLine();
+        while (!input.equals("END")) {
+            String[] data = input.split("\\s+");
+            switch (data[0]) {
+                case "Add":
+                    list.add(data[1]);
+                    break;
+                case "Remove":
+                    list.remove(Integer.parseInt(data[1]));
+                    break;
+                case "Contains":
+                    System.out.println(list.contains(data[1]));
+                    break;
+                case "Swap":
+                    list.swap(Integer.parseInt(data[1]), Integer.parseInt(data[2]));
+                    break;
+                case "Greater":
+                    System.out.println(list.countGreaterThan(data[1]));
+                    break;
+                case "Max":
+                    System.out.println(list.getMax());
+                    break;
+                case "Min":
+                    System.out.println(list.getMin());
+                    break;
+                case "Print":
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println(list.get(i));
+                    }
+                    break;
+            }
+            input = scan.nextLine();
+        }
+    }
+}
